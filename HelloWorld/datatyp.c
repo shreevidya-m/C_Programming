5dc220e7cdbcad8125d6c31debe99759c984804a
shreevidya-m <shreevidyasundar14@gmail.com>
2025-07-06 23:26:58 +0530

    Delete HelloWorld directory

--- HelloWorld/datatyp.c
+++
@@ -1,9 +0,0 @@
-#include "datatypes.h"
-
-void displayData() {
-    int a = 5;
-    float b = 8.9;
-    char gen = 'F';
-
-    printf("%d %.2f %c\n", a, b, gen);  // Correct format specifiers
-}
--- HelloWorld/datatypes.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
- int a = 5;
- float b= 8.9;
- char gen= 'F';
- printf("%d %.2f %c\n", a, b, gen);
- return 0;
-}
